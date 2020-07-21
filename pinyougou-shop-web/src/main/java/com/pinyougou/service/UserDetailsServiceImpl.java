78a0ccc57d3c756f8ea36428960b637dcc87ef0d
Mechanic <zhangchenqin@126.com>
2020-07-21 17:55:03 +0800

    20200721 公司提交

--- pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pinyougou.service;
 
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -11,12 +13,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private SellerService sellerService;
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println("经过了UserDetailsServiceImpl");
         List<GrantedAuthority> authorities = new ArrayList<>();
         GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_SELLER");
         authorities.add(authority);
-        return new User(username,"123123",authorities);
+        TbSeller seller = sellerService.findOne(username);
+        if (seller != null) {
+            if ("1".equals(seller.getStatus())) {
+                return new User(username,seller.getPassword(),authorities);
+            }
+        }
+        return null;
     }
 }
