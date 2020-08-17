de4266992cb327067342a07401ceae661a0f8881
Mechanic <zhangchenqin@126.com>
2020-08-17 18:18:33 +0800

    co.202008171818

---
+++ pinyougou-solr-util/src/main/java/util/TransforItem.java
@@ -0,0 +1,46 @@
+package util;
+
+import com.alibaba.fastjson.JSON;
+import com.pinyougou.mapper.TbItemMapper;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.pojo.TbItemExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class TransforItem {
+
+    @Autowired
+    TbItemMapper tbItemMapper;
+
+    @Autowired
+    SolrTemplate solrTemplate;
+
+    public void transfor() {
+        TbItemExample tbItemExample = new TbItemExample();
+        TbItemExample.Criteria criteria = tbItemExample.createCriteria();
+        criteria.andStatusEqualTo("1");
+        List<TbItem> tbItems = tbItemMapper.selectByExample(tbItemExample);
+
+        for (TbItem item : tbItems) {
+            System.out.println(item.getId()+" | "+item.getTitle()+" | "+item.getPrice());
+            Map spec = JSON.parseObject(item.getSpec(), Map.class);
+            item.setSpecMap(spec);
+        }
+        solrTemplate.saveBeans(tbItems);
+        solrTemplate.commit();
+
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath*:spring/applicationContext*.xml");
+        TransforItem transforItem = (TransforItem) classPathXmlApplicationContext.getBean("transforItem");
+        transforItem.transfor();
+    }
+
+}
